ae968e02c5d6b8e8cc55e184e0d7e44ff7717b03
saroj789 <sarojkumarvaishya@gmail.com>
2022-04-22 01:23:48 +0530

    config

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ["ecommerse-django-env.eba-8fek4qis.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["ecommerse-django-env.eba-8fek4qis.us-west-2.elasticbeanstalk.com",'localhost']
 
 
 # Application definition

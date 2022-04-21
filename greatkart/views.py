9740e6c00086c3c681d5037ec54a9f10c9e03d50
saroj789 <sarojkumarvaishya@gmail.com>
2022-04-21 15:00:58 +0530

    allowed host and db config

--- greatkart/views.py
+++ greatkart/views.py
@@ -4,6 +4,7 @@ from store.models import Product, ReviewRating
 def home(request):
   products = Product.objects.all().filter(is_available=True).order_by('created_date')
 
+  reviews = None
   for product in products:
     reviews = ReviewRating.objects.filter(product_id=product.id, status=True)     # make it false when you daont want to show that review
 

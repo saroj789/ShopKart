b38593a61c15b94fac53535399547c97b2fff6f5
saroj789 <sarojkumarvaishya@gmail.com>
2022-04-22 23:49:05 +0530

    boto3&storages(s3 buket for static& media files)

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -147,9 +148,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = ['greatkart/static' ]  
+USE_S3 = config('USE_S3', default=True, cast=bool)
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE',default=False, cast=bool)
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_DIRS = ['greatkart/static' ]
+
+    # meadia storage
+    DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
+
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR /'static'
+    STATICFILES_DIRS = ['greatkart/static' ]
     # we can put path of static files of applevel
 
 
